9976e10b0fb0be1b73f8e1b73527d089ecb17342
Sri Kumar <kssrikumar180703@gmail.com>
2023-11-10 16:36:35 +0530

    added main run init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import initialize
+import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -16,6 +17,6 @@ app.add_middleware(
 )
 
 
-
-
 app.include_router(initialize.router)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
