639d8eaa6e3f180fce54839ecbabbb6cf821a3e4
rushi <rushivardhan18@gmail.com>
2023-11-26 16:51:31 +0530

    fixed overallperfromance,added uploadsoftskill added carrer and target values

--- service/importCSV.py
+++ service/importCSV.py
@@ -71,3 +71,42 @@ def convertMarks(file, markSys, year, semester):
         if email != "False":
             emails.append(email)
     return emails
+
+def check_student_exists(year, semester):
+    student = db.colection1.find_one({'year': year, 'semester': semester})
+    return student is not None
+
+def add_student_to_studentdetails_collection(student_details):
+    db.collection1.insert_one(student_details)
+
+def add_marks_to_softskill_collection(studentId, subjectMarks):
+    db.collection5.insert_one({'student_id': studentId, 'marks_data': subjectMarks})
+
+def parse_excel(file):
+    
+    for index in range(len(file)):
+        userName = file.iloc[index]["STUDNAME"]
+        id = file.iloc[index]["STUDENTID"]
+        email = file.iloc[index]["EMAIL"]
+        for column in file.columns:
+            if column in ["S NO", "STUDNAME", "STUDENTID", "EMAIL", "PRGM"]:
+                student_details = file.iloc[index]["S NO", "STUDNAME", "STUDENTID", "EMAIL", "PRGM"]
+                continue
+            else:
+                subjectMarks = file.iloc[index][column]
+        return student_details,subjectMarks
+
+def uploadSoftSkill(file, year, semester):
+    student_details, subjectMarks = parse_excel(file)
+
+    student_exists = check_student_exists(year, semester)
+
+    if student_exists:
+        student = db.studentdetails.find_one({'year': year, 'semester': semester})
+        add_marks_to_softskill_collection(student['_id'], marks_data)
+    else:
+        add_student_to_studentdetails_collection(student_details)
+
+        new_student = db.studentdetails.find_one({'year': year, 'semester': semester})
+        add_marks_to_softskill_collection(new_student['_id'], marks_data)
+
