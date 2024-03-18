b9fe49f8266c0eb3fa3ab4a7ab39092950f07638
Sri Kumar <kssrikumar180703@gmail.com>
2024-03-18 17:07:52 +0530

    added industry

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ app.include_router(analysis.router)
 app.include_router(Questionrouter.router)
 app.include_router(JobRouter.router)
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- service/analysisService.py
+++ service/analysisService.py
@@ -2,6 +2,8 @@ import csv
 from pathlib import Path
 from bson import ObjectId
 import db
+import google.generativeai as genai
+
 def calculate_student_skill_gap(result_list1,studentId):
 
 
@@ -370,40 +372,53 @@ def recommendations(result_list1,studentId):
                 mark_gaps[subject] = 0
 
     recommendations = {}
+    print(mark_gaps)
+    genai.configure(api_key="AIzaSyAJBkjAGESGhwiwaHwET9NTb5rfu6gYnsM")
 
+    # Set up the model
+    generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 1000,
+    }
+
+    safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },    
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    ]
+
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro",
+                                generation_config=generation_config,
+                                safety_settings=safety_settings)
+
+    convo = model.start_chat(history=[
+    ])
+
+    convo.send_message(
+        "My semester marks in these subjects are following, if i want to become a full stack dev where do i need to improve in every subject"+str(mark_gaps)+
+        "Output format: [{'<subject_name>':<recommendations[str]>}]"
+        )
+    return(convo.last.text)
     for subject, gap in mark_gaps.items():
         if gap >= 0:
             suggestions = []
-            if gap > 50:
-                suggestions.extend(
-                    [
-                        f"Consider Basics to intermediate courses in {subject}",
-                        f"Explore real-world projects in {subject}",
-                        f"Study more insights through practical implementations {subject}",
-                        f"Work more on problem solving and logical thinking related to {subject}",
-                        f"Be more focused on {subject} to achieve your dream job",
-                    ]
-                )
-
-            elif 25 < gap <= 50:
-                suggestions.extend(
-                    [
-                        f"Consider advanced courses in {subject}",
-                        f"Explore real-world projects in {subject}",
-                        f"Study more insights through practical implementations {subject}",
-                    ]
-                )
-
-            elif 25 > gap < 0:
-                suggestions.extend(
-                    [
-                        f"Brush up on {subject} wisely",
-                        f"Practice more problems in {subject} in HackerRank or other platforms to keep consistency",
-                    ]
-                )
-
-            elif gap == 0:
-                continue
+            suggestions.extend([])
+             
 
             recommendations[subject] = suggestions
 
